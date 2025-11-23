c2a48add41b3e3c65a7962f271225df42888e3bb
Rahul Metre <34718002+RahulMetre03@users.noreply.github.com>
2025-11-24 01:31:10 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -9,17 +9,14 @@ import { getAndRedirect } from "./src/controllers/link.controller.js";
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.get("/healthz", (req, res) => {
+  res.status(200).json({ ok: true, version: "1.0" });
+});
 
 // Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/links", linkRoutes);
 app.get("/:code", getAndRedirect);
-
-// Health check endpoint
-app.get("/healthz", (req, res) => {
-  res.status(200).json({ ok: true, version: "1.0" });
-});
-
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, "0.0.0.0", () => {
