5ebfc7ce51d9bddf6a903b6c89a52674b3566e4f
Rahul Metre <34718002+RahulMetre03@users.noreply.github.com>
2025-11-25 21:34:45 +0530

    Routing issue

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 
 import Login from "./pages/Login.jsx";
 import Signup from "./pages/Signup.jsx";
@@ -15,6 +15,7 @@ const App = () => {
       <Route path="/dashboard" element={<Dashboard />} />
       <Route path="/stats" element={<StatsPage />} />
       <Route path="/code/:code" element={<LinkStatsPage />} />
+      <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
   );
 };
